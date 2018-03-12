4bae4690d21677d95a00f5fdd199e3eaa9ed520e
zuzhi <476513451@qq.com>
2018-03-12 17:43:43 +0800

    Initial commit

---
+++ backend/src/main/java/com/zuzhi/springbootvuejsproject/SpringBootVuejsProjectApplication.java
@@ -0,0 +1,12 @@
+package com.zuzhi.springbootvuejsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVuejsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsProjectApplication.class, args);
+	}
+}
