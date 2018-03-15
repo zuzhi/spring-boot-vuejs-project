8c85b606374b3d9a932bb1a9e311d394951f132d
zuzhi <476513451@qq.com>
2018-03-15 10:48:40 +0800

    Add two REST APIs: Book API, Rating API

---
+++ backend/src/main/java/com/zuzhi/springbootvuejsproject/rating/RatingNotFoundException.java
@@ -0,0 +1,19 @@
+package com.zuzhi.springbootvuejsproject.rating;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * RatingNotFoundException
+ *
+ * @author zuzhi
+ * @date 14/03/2018
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class RatingNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 518040114997784430L;
+
+    RatingNotFoundException(String message) {
+        super(message);
+    }
+}
